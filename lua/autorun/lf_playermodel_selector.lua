ba83809e5f176b499d0f093958cd67add386d89c
Fesiug <publicfesiug@outlook.com>
2023-01-16 15:12:57 -0500

    final touches

--- lua/autorun/lf_playermodel_selector.lua
+++ lua/autorun/lf_playermodel_selector.lua
@@ -380,7 +380,7 @@ end
 if CLIENT then
 
 
-local Version = "3.3 (Edit 3)"
+local Version = "3.3, Fesiug's Edit"
 local Menu = { }
 local Frame
 local default_animations = { "idle_all_01", "menu_walk", "pose_standing_02", "pose_standing_03", "idle_fist" }
